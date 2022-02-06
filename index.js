4dce4aa9747ef354b8d9ece4c1d9fa736b9d0ee8
iMadfxq <imadfxq@gmail.com>
2022-02-06 16:54:46 -0600

    Added event delegation and finished with styles

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const popupContainer = document.querySelector('.popup__container')
 const contentContainer = document.querySelector('.content__container')
 const openButton = contentContainer.querySelector('button')
 const closeButton = popupContainer.querySelector('span')
+const main = document.querySelector('main')
 
-openButton.addEventListener('click', () => {
-  popupContainer.style.display = 'grid'
-})
-closeButton.addEventListener('click', () => {
-  popupContainer.style.display = 'none'
+main.addEventListener('click', (e) => {
+  if(e.target == openButton) {
+    popupContainer.style.display = 'flex'
+  } else if (e.target == closeButton || e.target == popupContainer) {
+    popupContainer.style.display = 'none'
+  }
 })
